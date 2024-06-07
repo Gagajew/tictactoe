bb93e6da96d2428ff8304eded11ba04087fbfba9
Maryamhassan21 <wi23b108@technikum-wien.at>
2024-06-07 10:06:21 +0200

    Tictactoe main code ertsellt

--- BliBlub/src/main/java/org/example/Main.java
+++ BliBlub/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
 
-    }
+        constructor() {
+            this.player1 = new Player('X');
+            this.player2 = new Player('O');
+            this.currentPlayer = this.player1;
+            this.board = new Board();
+        }
+
+        start() {
+            this.board.clear(); // Ensures the board is clear before starting
+            // Game loop can be implemented here
+        }
+
+        switchCurrentPlayer() {
+            this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+        }
+
+        hasWinner() {
+            // This method should be implemented to check for a winner
+            return false; // Placeholder
+        }
 }
