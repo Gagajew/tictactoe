c3f67283a572a9e23535fb7f22978d9cb8c6bf98
Maryamhassan21 <wi23b108@technikum-wien.at>
2024-06-07 10:33:50 +0200

    Tictactoe class überarbeitet

--- BliBlub/src/main/java/org/example/Tictactoe.java
+++ BliBlub/src/main/java/org/example/Tictactoe.java
@@ -1,24 +1,28 @@
 package org.example;
 
 public class Tictactoe {
+    Player player1;
+    Player player2;
+    Player currentPlayer;
+    Board board;
 
-        constructor() {
+        void constructor() {
             this.player1 = new Player('X');
             this.player2 = new Player('O');
             this.currentPlayer = this.player1;
             this.board = new Board();
         }
 
-        start() {
+        void start() {
             this.board.clear(); // Ensures the board is clear before starting
             // Game loop can be implemented here
         }
 
-        switchCurrentPlayer() {
+        void switchCurrentPlayer() {
             this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
         }
 
-        hasWinner() {
+        boolean hasWinner() {
             // This method should be implemented to check for a winner
             return false; // Placeholder
         }
