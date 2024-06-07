34d4ebdf50a56344b4733db63c043ca38b86ef49
katharinaschattner <wi23b097@technikum-wien.at>
2024-06-07 10:13:07 +0200

    Board Code erstellt

--- BliBlub/src/main/java/org/example/Board.java
+++ BliBlub/src/main/java/org/example/Board.java
@@ -1,4 +1,39 @@
 package org.example;
 
 public class Board {
+    classBoard {
+        constructor() {
+            this.cells = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+        ];
+        }
+
+        isCellEmpty(x, y) {
+            returnthis.cells[x][y] === ' ';
+        }
+
+        place(x, y, marker) {
+            if (this.isCellEmpty(x, y)) {
+                this.cells[x][y] = marker;
+            }
+        }
+
+        isFull() {
+            returnthis.cells.flat().every(cell => cell !== ' ');
+        }
+
+        clear() {
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    this.cells[i][j] = ' ';
+                }
+            }
+        }
+
+        print() {
+            console.log(this.cells.map(row => row.join('|')).join('\n'));
+        }
+    }
 }
