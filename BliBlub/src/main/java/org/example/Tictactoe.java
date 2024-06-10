c1f18f15d83f92daa226556d4d013ec9a4aecc84
Gagajew <wi23b164@technikum-wien.at>
2024-06-10 19:35:23 +0200

    Klasse Tictactoe Code wurde durchkommentiert

--- BliBlub/src/main/java/org/example/Tictactoe.java
+++ BliBlub/src/main/java/org/example/Tictactoe.java
@@ -2,67 +2,68 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Tictactoe {
-    Player player1;
-    Player player2;
-    Player currentPlayer;
-    Board board;
+public class Tictactoe { //public Methode Tictactoe
+    Player player1; //Variable player1 vom Typ Player
+    Player player2; //Variable player2 vom Typ Player
+    Player currentPlayer; //Variable currentPlayer vom Typ Player
+    Board board; //Variable board vom Typ Board
 
-    public Tictactoe() {
-        this.player1 = new Player('X');
-        this.player2 = new Player('O');
-        this.currentPlayer = this.player1;
-        this.board = new Board();
+    public Tictactoe() { //public Konstruktor Tictactoe
+        this.player1 = new Player('X'); //marker X für player1 zugewiesen
+        this.player2 = new Player('O'); //marker O für player2 zugewiesen
+        this.currentPlayer = this.player1; //erster Spieler ist player1
+        this.board = new Board(); //dem Board wird die Methode Board() zugewiesen
     }
 
     public void start() {
         System.out.println("The game has started!");
-        this.board.clear(); // Ensures the board is clear before starting
+        this.board.clear(); // Das Board ist leer beim Spielstart
 
-        hasWinner();
+        hasWinner(); //Methode wird bei Start aufgerufen
     }
 
     public void switchCurrentPlayer() {
         this.currentPlayer = (this.currentPlayer == this.player1) ? this.player2 : this.player1;
+        //Die Spieler werden gewechselt
 
     }
 
-        public boolean hasWinner() {
+        public boolean hasWinner() { //Methode zur Überprüfung ob es einen Gewinner gibt und wer gewonnen hat
             Scanner scanner = new Scanner(System.in);
             boolean winner = false;
 
 
-            while (!board.isFull() && !winner){
-                this.board.print();
-                System.out.println("Current player: " + this.currentPlayer.getMarker());
+            while (!board.isFull() && !winner){ //während das Board leer ist und es keinen Gewinner gibt
+                this.board.print(); //wird das Board dargestellt
+                System.out.println("Current player: " + this.currentPlayer.getMarker()); //der Spieler, der an der Reihe ist, wird angezeigt
 
-                int row,  col;
+                int row,  col; //Spalte und Reihe definiert als Integer
                 while (true){
-                    System.out.print("Enter row (0, 1, 2):");
-                    row = scanner.nextInt();
+                    System.out.print("Enter row (0, 1, 2):"); //Ausgabe des Textes in der Klammer
+                    row = scanner.nextInt(); //Eingabe der Reihe
                     System.out.print("Enter col (0, 1, 2):");
-                    col = scanner.nextInt();
+                    col = scanner.nextInt(); //Eingabe der Zeile
 
-                    if (row >= 0 && row < 3 && col >= 0 && col < 3 && board.isCellEmpty(row, col)){
-                        break;
+                    if (row >= 0 && row < 3 && col >= 0 && col < 3 && board.isCellEmpty(row, col)){ //wenn der eingegebene Wert innerhalb der Matrix liegt und das Feld leer ist
+                        break; //wird die Schleife aufgehalten
                     }
                     else{
-                        System.out.println("Invalid input! Please try again.");
+                        System.out.println("Invalid input! Please try again."); //sonst kommt die Meldung
                     }
                 }
 
-                board.place(row, col, currentPlayer.getMarker());
+                board.place(row, col, currentPlayer.getMarker()); //marker wird entsprechend gesetzt
 
-                if (board.checkWin(currentPlayer.getMarker())){
-                    winner = true;
-                    this.board.print();
-                    System.out.println("Player " + this.currentPlayer.getMarker() + " won!");
+                if (board.checkWin(currentPlayer.getMarker())){ //wenn der gesetzte Marker richtig gesetzt wurde und einen Gewinner markiert
+                    winner = true; //wird ein Gewinner bestimmt
+                    this.board.print(); //das board wird noch dargestellt
+                    System.out.println("Player " + this.currentPlayer.getMarker() + " won!"); //Gewinner wird angezeigt
                 }
                 else{
-                    switchCurrentPlayer();
+                    switchCurrentPlayer(); //sonst wird der Spieler gewechselt
                 }
             }
-            if (!winner){
+            if (!winner){ //wenn es keinen Gewinner gibt
                 System.out.println("Wow...it's a draw!");
             }
 
