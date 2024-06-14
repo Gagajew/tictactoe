70436835b91beb642f8c7e7f82db6a2ed4b0556f
katharinaschattner <wi23b097@technikum-wien.at>
2024-06-14 13:22:49 +0200

    Board Code verbessert

--- BliBlub/src/test/java/org/example/BoardTest.java
+++ BliBlub/src/test/java/org/example/BoardTest.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
+    @Test
+    void isCellEmpty() {
+    }
+
+    @Test
+    void place() {
+    }
+
+    @Test
+    void isFull() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void checkWin() {
+    }
 }
